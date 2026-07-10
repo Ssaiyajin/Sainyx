36036d6d343ff3cd190d692959253a7045f632a3
Nihar Sawant <NIharswaantrockstar2013@gamil.com>
2026-07-11 01:29:54 +0200

    fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import io
-from turtle import pd
+import pandas as pd
 import torch
 from model.gpt import Sainyx, BLOCK_SIZE
 from flask import Flask, render_template, request, jsonify, send_file
